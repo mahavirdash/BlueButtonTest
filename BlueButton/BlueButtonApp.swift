bb96db60719357c5f35870f076feed085eae8a44
Mahavir Dash <mahavirdash2017@gmail.com>
2023-06-07 21:28:46 +0530

    Initial Commit

---
+++ BlueButton/BlueButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueButtonApp.swift
+//  BlueButton
+//
+//  Created by Mahavir Ganapati Dash on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BlueButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
